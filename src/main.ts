5316d36880eefe4a30d9893d2dcc655f7b5fe9a4
Fahmi Dzulqarnain <60492666+fahmi-dzulqarnain@users.noreply.github.com>
2022-11-15 22:14:19 +0800

    Activate SSL

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import { TransformInterceptor } from './transform.interceptor';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  // const privateKey = fs.readFileSync('/etc/ssl/private/private.key', 'utf8');
-  // const certificate = fs.readFileSync('/etc/ssl/certificate.crt', 'utf8');
-  // const httpsOptions = { key: privateKey, cert: certificate };  
-  // const app = await NestFactory.create(AppModule, {httpsOptions});
-  const app = await NestFactory.create(AppModule);
+  const privateKey = fs.readFileSync('/etc/ssl/private/private.key', 'utf8');
+  const certificate = fs.readFileSync('/etc/ssl/certificate.crt', 'utf8');
+  const httpsOptions = { key: privateKey, cert: certificate };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  // const app = await NestFactory.create(AppModule);
 
   app.enableCors()
   app.useGlobalInterceptors(new TransformInterceptor())
