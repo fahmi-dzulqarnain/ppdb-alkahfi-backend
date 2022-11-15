e87020702838488f45730a17e219619a32b070e3
fahmi-dzulqarnain <fdz.fhz1302@gmail.com>
2022-11-15 07:25:14 +0000

    add ssl

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const privateKey = fs.readFileSync('/etc/ssl/private/private.key', 'utf8');
+  const certificate = fs.readFileSync('/etc/ssl/certificate.crt', 'utf8');
+  const httpsOptions = { key: privateKey, cert: certificate };  
+  const app = await NestFactory.create(AppModule, {httpsOptions});
 
   app.enableCors()
   app.useGlobalInterceptors(new TransformInterceptor())
